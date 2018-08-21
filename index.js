a42cf819d458b9e71fb794b578a8eb37caea821e
Petr Kosikhin <petr@hanzo.es>
2018-08-22 00:26:19 +0300

    explicitly check for undefined values to allow false values to be considered if truthy value is default

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 
-const generateSecretsOrExtWithDefault = (extConfig = {}) => (def) => (propName) => extConfig[propName] || process.env[propName] || def || '';
+const isUndef = value => typeof value === 'undefined';
+
+const generateSecretsOrExtWithDefault = (
+  extConfig = {}
+) => (
+  def
+) => (
+  propName
+) => {
+  let toReturn;
+  if (!isUndef(extConfig[propName])) {
+    toReturn = extConfig[propName];
+  } else if (!isUndef(process.env[propName])) {
+    toReturn = process.env[propName];
+  } else if (!isUndef(def)) {
+    toReturn = def;
+  } else {
+    toReturn = '';
+  }
+  return toReturn;
+};
 
 const wrapWithPropNames = obj => Object.keys(obj)
   .reduce(
     (
       acc,
-        key
+      key
     ) => typeof obj[key] === 'function'
   ? Object.assign({}, acc, { [key]: obj[key](key) })
   : Object.assign({}, acc, { [key]: obj[key] }),
